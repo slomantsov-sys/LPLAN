eda21c28a8c3bc4937f26dfceea1ee3e522beeb2
slomantsov-sys <slomantsov@gmail.com>
2026-06-07 00:33:26 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,7 @@
+// Service Worker - always fetch fresh from network
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request, { cache: 'no-store' })
+      .catch(() => caches.match(event.request))
+  );
+});
